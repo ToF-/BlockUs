770972b7c47bb52ddbbb66309cdcc094e8be927a
Christophe Thibaut <cthibaut@octo.com>
2012-09-05 08:20:24 +0200

    Board doesn't accept a shape outside limits (fake)

--- Blockus/Board.hs
+++ Blockus/Board.hs
@@ -1,2 +1,15 @@
 module Blockus.Board
 where
+import Blockus.Shape
+
+data BoardError = IllegalCoord
+  deriving(Eq,Show)
+
+data Board      = Nil
+  deriving(Eq,Show)
+
+emptyBoard :: Board
+emptyBoard = Nil
+
+accept :: Board -> Coord -> Shape -> Either BoardError Board
+accept _ _ _ = Left IllegalCoord
--- Blockus/Tests/TestBoard.hs
+++ Blockus/Tests/TestBoard.hs
@@ -5,7 +5,13 @@ import Test.HUnit
 import Blockus.Shape
 import Blockus.Board
 
-tests = "A Board" ~: TestList 
-  []
 
+tests = "A Board" ~: TestList 
+  ["doesn't accept the dropping outside its limit" ~: 
+    TestList ["of a very single square shape " ~: 
+                accept emptyBoard (0,0) block ~?= Left IllegalCoord
+            ]
+  ]
+
+block = shape [(0,0)]
 
