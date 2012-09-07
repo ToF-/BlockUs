d9a57b7d4e0f0158f7413864201c125e48c30dfa
ToF- <cthibaut@octo.com>
2012-09-07 21:57:44 +0200

    repair TestAll

--- TestAll.hs
+++ TestAll.hs
@@ -6,10 +6,7 @@ import Blockus.Tests.TestPiece
 import Blockus.Tests.TestBoard
 
 main = runTestTT $ TestList
-<<<<<<< HEAD
   [Blockus.Tests.TestShape.tests
+  ,Blockus.Tests.TestPiece.tests
   ,Blockus.Tests.TestBoard.tests
-  ,Blockus.Tests.TestPiece.tests]
-=======
-  [Blockus.Tests.TestShape.tests]
->>>>>>> commit w/o sync
+  ]
