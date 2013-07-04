837a3a1be59b9853ce45f2d2a2c0b01ed032378d
ToF- <cthibaut@octo.com>
2013-07-04 15:27:34 +0200

    added filling of board with pieces and retrieving colors in board cells

--- Blockus/Board.hs
+++ Blockus/Board.hs
@@ -2,16 +2,24 @@ module Blockus.Board
 where
 import Blockus.Shape
 import Blockus.Piece
+import qualified Data.Set as S
 
 data BoardError = IllegalCoord
   deriving(Eq,Show)
 
-data Board      = Nil
-  deriving(Eq,Show)
+type Board      = [(Coord,Color)] 
 
 emptyBoard :: Board
-emptyBoard = Nil
+emptyBoard = []
 
 place :: Board -> Coord -> Piece -> Either BoardError Board
 place _ (0,0) _ = Left IllegalCoord
-place b _ _     = Right b
+place b c p     = Right (put b c p)
+
+put :: Board -> Coord -> Piece -> Board
+put b (x,y) (shape,color) = b ++ zip (S.toList (translate (x,y) shape)) (repeat color)
+
+at :: Board -> Coord -> Maybe Color
+at = flip lookup
+
+		 
--- Blockus/Tests/TestBoard.hs
+++ Blockus/Tests/TestBoard.hs
@@ -8,7 +8,8 @@ import Blockus.Piece
 import Blockus.Board
 
 
-blueBlock = piece (shape [(0,0)]) Blue
+blueBlock = piece block Blue
+redBlock = piece block Red
 
 tests = "A Board" ~: TestList 
   ["doesn't accept the placing outside its limit" ~: 
@@ -17,8 +18,37 @@ tests = "A Board" ~: TestList
             ]
   ,"accepts a placing in correct initial position" ~:
     TestList ["of a blue simple piece" ~:
-                check $ isRight $ place emptyBoard (1,1) blueBlock 
-              ]            
+                check $ isRight $ place emptyBoard (1,1) blueBlock
+             ,"and it can be detected on the board" ~:
+                let board = case place emptyBoard (1,1) blueBlock of
+                	Right b -> b 
+                	Left _  -> error "test error"
+                in board `at` (1,1) ~?= Just Blue
+              ]
+  ,"knows what is its cells" ~:
+  	TestList ["when it's empty" ~:
+  				emptyBoard `at` (1,1) ~?= Nothing
+  		     ,"when it's filled with one block" ~:
+  		        let board = put emptyBoard (1,1) blueBlock
+  		        in board `at` (1,1) ~?= Just Blue
+  		     ,"when it's filled with one block at any place" ~:
+  		        let board = put emptyBoard (3,4) blueBlock
+  		        in board `at` (3,4) ~?= Just Blue
+  		     ,"when it's filled with one block of any color" ~:
+  		        let board = put emptyBoard (3,4) redBlock
+  		        in board `at` (3,4) ~?= Just Red
+  		     ,"when it's filled with a piece of several blocks" ~:
+  		        let board = put emptyBoard (2,2) (piece (shape [(0,0),(1,0),(0,1)]) Yellow)
+  		        in TestList [board `at` (2,2) ~?= Just Yellow
+  		        	 		,board `at` (2,3) ~?= Just Yellow
+  		        	 		,board `at` (3,2) ~?= Just Yellow
+  		        	]
+  		     ,"when it's filled with several pieces" ~:
+  		     	let board' = put emptyBoard (1,1) redBlock
+  		     	    board = put board' (2,2) blueBlock
+  		     	in TestList [board `at` (1,1) ~?= Just Red
+  		     				,board `at` (2,2) ~?= Just Blue]
+  		     ]            
   ]
 
 isRight :: Either a b -> Bool
