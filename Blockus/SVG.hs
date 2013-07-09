85a14a6178b059104e8d557f1f729d24383b64fa
ToF- <cthibaut@octo.com>
2013-07-09 23:16:40 +0200

    refactoring SVG

--- Blockus/SVG.hs
+++ Blockus/SVG.hs
@@ -1,19 +1,32 @@
 module Blockus.SVG
 where
+import Data.List
 
 data RGB = RGB Int Int Int 
-data Context = Context RGB Int RGB
-attribute :: String -> String ->String
-attribute a v = " " ++ a ++ "=" ++ show v
+data Context = Context { fillColor :: RGB, strokeWidth :: Int, strokeColor :: RGB }
 
-style :: RGB -> Int -> RGB -> String
-style fc w sc = "fill:"++rgb fc ++ ";stroke-width:" ++ show w ++ ";stroke:" ++ rgb sc
-  where
-      rgb (RGB r g b) = "rgb(" ++ show r ++ "," ++ show g ++ "," ++ show b ++ ")"
+attribute :: String -> String ->String
+attribute a v = (' ': a) ++ "=" ++ show v
+
+style :: Context -> String
+style ctx = showArgs ";" $ map (uncurry styleAttribute) [("fill",          rgb (fillColor ctx))
+										 				,("stroke-width",  show (strokeWidth ctx))
+										 				,("stroke",        rgb (strokeColor ctx))]
+
+rgb (RGB r g b) = "rgb(" ++ showArgs "," (map show [r,g,b]) ++ ")"
+
+styleAttribute :: String -> String -> String
+styleAttribute a v = a ++ ":" ++ v
+
+showArgs :: String -> [String] -> String
+showArgs sep args = concat $ intersperse sep $ args
+
+element :: String -> [String] -> String
+element el atts = ('<' : el) ++ concat atts ++ "/>"
 
 rect :: Int -> Int -> Int -> Int -> Context -> String
-rect x y w h (Context fc sw sc) = "<rect" ++ attribute "x" (show x) ++ 
-                              attribute "y" (show y) ++ 
-                              attribute "width" (show w) ++
-                              attribute "height" (show h) ++
-                              attribute "style" (style fc sw sc) ++ "/>"
+rect x y w h ctx = element "rect" (map (uncurry attribute)  [("x",     show x) 
+								                            ,("y",     show y) 
+								                            ,("width", show w)
+								                            ,("height",show h)
+								                            ,("style", style ctx)] )
--- Blockus/Tests/TestSVG.hs
+++ Blockus/Tests/TestSVG.hs
@@ -6,7 +6,7 @@ import Blockus.SVG as S
 
 tests = "A SVG" ~: TestList
   ["defines attributes" ~: S.attribute "X" "12" ~?= " X=\"12\""
-  ,"defines style" ~: S.style (S.RGB 10 20 0) 4 (S.RGB 20 80 120) ~?= "fill:rgb(10,20,0);stroke-width:4;stroke:rgb(20,80,120)"
+  ,"defines style" ~: S.style (S.Context (S.RGB 10 20 0) 4 (S.RGB 20 80 120)) ~?= "fill:rgb(10,20,0);stroke-width:4;stroke:rgb(20,80,120)"
   ,"creates a rect" ~: let context = S.Context (S.RGB 0 0 255) 1 (S.RGB 0 0 0)
   						in S.rect 10 15 23 50 context ~?= 
   							"<rect x=\"10\" y=\"15\" width=\"23\" height=\"50\" style=\"fill:rgb(0,0,255);stroke-width:1;stroke:rgb(0,0,0)\"/>"
