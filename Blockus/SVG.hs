e3d298c2c4ad91c0ea85d2c0a3cffb75d832e4e8
ToF- <cthibaut@octo.com>
2013-07-09 13:10:17 +0200

    SVG draws a rect

--- Blockus/SVG.hs
+++ Blockus/SVG.hs
@@ -2,11 +2,18 @@ module Blockus.SVG
 where
 
 data RGB = RGB Int Int Int 
-
+data Context = Context RGB Int RGB
 attribute :: String -> String ->String
-attribute a v = " " ++ a ++ " = " ++ show v
+attribute a v = " " ++ a ++ "=" ++ show v
 
 style :: RGB -> Int -> RGB -> String
-style fc w sc = attribute "style" $ "fill:"++rgb fc ++ ";stroke-width:" ++ show w ++ ";stroke:" ++ rgb sc
+style fc w sc = "fill:"++rgb fc ++ ";stroke-width:" ++ show w ++ ";stroke:" ++ rgb sc
   where
       rgb (RGB r g b) = "rgb(" ++ show r ++ "," ++ show g ++ "," ++ show b ++ ")"
+
+rect :: Int -> Int -> Int -> Int -> Context -> String
+rect x y w h (Context fc sw sc) = "<rect" ++ attribute "x" (show x) ++ 
+                              attribute "y" (show y) ++ 
+                              attribute "width" (show w) ++
+                              attribute "height" (show h) ++
+                              attribute "style" (style fc sw sc) ++ "/>"
