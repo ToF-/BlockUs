88fdff720f247dbc2f3865c29c62c2180cbd08f3
ToF- <cthibaut@octo.com>
2013-07-23 23:45:13 +0200

    added red SVG failing test

--- Blockus/SVGRenderer.hs
+++ Blockus/SVGRenderer.hs
@@ -4,5 +4,5 @@ where
 import Blockus.Piece
 
 render :: Int -> Int -> Piece -> String
-render _ _ _ = "<rect x=\"0\" y=\"0\" width=\"24\" height=\"24\" style=\"fill:rgb(0,0,255);stroke-width:2;stroke:rgb(0,0,240)\"/>"
+render _ _ _ = "<rect x=\"0\" y=\"0\" width=\"24\" height=\"24\" style=\"stroke-width:2;fill:#0000ff;stroke:#0000f0\"/>"
   		
--- Blockus/Tests/TestSVGRenderer.hs
+++ Blockus/Tests/TestSVGRenderer.hs
@@ -6,9 +6,14 @@ import Blockus.Piece
 import Blockus.Shape
 import Blockus.SVGRenderer
 
+b = pieceFrom ["#"] Blue
+r = pieceFrom ["#"] Red 
+
 tests = "A SVG renderer" ~: TestList
   ["renders the SVG drawing of a piece" ~:
-  	let p = pieceFrom ["#"] Blue
-  	in (render 0 0 p) ~?= 
-  		"<rect x=\"0\" y=\"0\" width=\"24\" height=\"24\" style=\"fill:rgb(0,0,255);stroke-width:2;stroke:rgb(0,0,240)\"/>"
-  		]
+	 (render 0 0 b) ~?= 
+  		"<rect x=\"0\" y=\"0\" width=\"24\" height=\"24\" style=\"stroke-width:2;fill:#0000ff;stroke:#0000f0\"/>"
+  ,"renders pieces of different colors" ~:
+	 (render 0 0 b) ~?= 
+  		"<rect x=\"0\" y=\"0\" width=\"24\" height=\"24\" style=\"stroke-width:2;fill:#00ff00;stroke:#00f000\"/>"
+  ]
