944e3d844cd850b6c0b4de7f4b44187c50220ead
ToF- <cthibaut@octo.com>
2013-07-24 22:41:56 +0200

    refactoring

--- Blockus/SVGRenderer.hs
+++ Blockus/SVGRenderer.hs
@@ -4,14 +4,16 @@ where
 import Blockus.Piece
 
 render :: Int -> Int -> Piece -> String
-render x y (_,c) = "<rect " ++ (coords x y) ++ size ++ style ++ "/>"
-		where color Blue = "fill:#0000ff;stroke:#0000f0"
-		      color Red  = "fill:#ff0000;stroke:#f00000"
-		      color Green= "fill:#00ff00;stroke:#00f000"
-		      color Yellow="fill:#ffff00;stroke:#f0f000"
-		      coords x y = int "x" (x*24) `space` int "y" (y*24)
+render x y (_,c) = "<rect " ++ (coords x y) ++ size ++ style c ++ "/>"
+		where coords x y = int "x" (x*24) `space` int "y" (y*24)
 		      size       = int " width" 24 `space` int "height" 24 
-		      style = param " style" ("stroke-width:2;" ++ color c)
+		      style c = param " style" ("stroke-width:2;" ++ "fill:" ++ fc ++ ";stroke:" ++ sc)
+		      	where (fc,sc) = colors c
+		      	      colors Blue = ("#0000ff","#0000f0")
+		              colors Red  = ("#ff0000","#f00000")
+		              colors Green= ("#00ff00","#00f000")
+		              colors Yellow=("#ffff00","#f0f000")
+		      	
 		      int p v = param p (show v)
 		      param p v = p ++ "=" ++ enquote v
 		      enquote s = quote ++ s ++ quote
