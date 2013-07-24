40eff83c839cc446684c6aac4c07267a662d1d7a
ToF- <cthibaut@octo.com>
2013-07-24 22:11:32 +0200

    refactoring

--- Blockus/SVGRenderer.hs
+++ Blockus/SVGRenderer.hs
@@ -4,8 +4,14 @@ where
 import Blockus.Piece
 
 render :: Int -> Int -> Piece -> String
-render _ _ (_,c) = "<rect x=\"0\" y=\"0\" width=\"24\" height=\"24\" style=\"stroke-width:2;"++ (color c) ++ "\"/>"
+render x y (_,c) = "<rect " ++ (coords x y) ++ size ++ style ++ "/>"
 		where color Blue = "fill:#0000ff;stroke:#0000f0"
 		      color Red  = "fill:#ff0000;stroke:#f00000"
 		      color Green= "fill:#00ff00;stroke:#00f000"
-		      color Yellow="fill:#ffff00;stroke:#f0f000" 
+		      color Yellow="fill:#ffff00;stroke:#f0f000"
+		      coords x y = param "x" (show (x*24)) ++ " " ++ param "y" (show (y*24))
+		      size = param " width" (show 24) ++ " " ++ param "height" (show 24) 
+		      style = param " style" ("stroke-width:2;" ++ color c)
+		      param p v = p ++ "=" ++ enquote v
+		      enquote s = quote ++ s ++ quote
+		      quote = "\""
--- Blockus/Tests/TestSVGRenderer.hs
+++ Blockus/Tests/TestSVGRenderer.hs
@@ -19,9 +19,16 @@ tests = "A SVG renderer" ~: TestList
   ["renders the SVG drawing of a piece" ~: 
    	(render 0 0 g) ~?= 
   	 "<rect x=\"0\" y=\"0\" width=\"24\" height=\"24\" style=\"stroke-width:2;fill:#00ff00;stroke:#00f000\"/>"
+ 
   ,"renders pieces of different colors" ~: TestList 
   	[check $ (render 0 0 g) `includes` "#00ff00;stroke:#00f000" 
   	,check $ (render 0 0 b) `includes` "#0000ff;stroke:#0000f0" 
   	,check $ (render 0 0 r) `includes` "#ff0000;stroke:#f00000" 
   	,check $ (render 0 0 y) `includes` "#ffff00;stroke:#f0f000"]
+ 
+  ,"renders pieces at different positions" ~: TestList
+    [check $ (render 1 0 g) `includes`  "x=\"24\""
+    ,check $ (render 0 2 g) `includes`  "y=\"48\""
+    ]
+
   ]
