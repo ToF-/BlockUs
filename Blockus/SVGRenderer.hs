a6dae66dac2ca4ae7f1edf51e8026546a82aa465
ToF- <cthibaut@octo.com>
2013-07-24 21:53:50 +0200

    svg four colors for squares

--- Blockus/SVGRenderer.hs
+++ Blockus/SVGRenderer.hs
@@ -6,4 +6,6 @@ import Blockus.Piece
 render :: Int -> Int -> Piece -> String
 render _ _ (_,c) = "<rect x=\"0\" y=\"0\" width=\"24\" height=\"24\" style=\"stroke-width:2;"++ (color c) ++ "\"/>"
 		where color Blue = "fill:#0000ff;stroke:#0000f0"
-		      color Red  = "fill:#00ff00;stroke:#00f000"
+		      color Red  = "fill:#ff0000;stroke:#f00000"
+		      color Green= "fill:#00ff00;stroke:#00f000"
+		      color Yellow="fill:#ffff00;stroke:#f0f000" 
--- Blockus/Tests/TestSVGRenderer.hs
+++ Blockus/Tests/TestSVGRenderer.hs
@@ -2,18 +2,26 @@ module Blockus.Tests.TestSVGRenderer
 where
 
 import Test.HUnit
+import Blockus.Tests.TestUtils
+import Data.List
 import Blockus.Piece
 import Blockus.Shape
 import Blockus.SVGRenderer
 
 b = pieceFrom ["#"] Blue
 r = pieceFrom ["#"] Red 
+y = pieceFrom ["#"] Yellow
+g = pieceFrom ["#"] Green
+
+includes = flip isInfixOf
 
 tests = "A SVG renderer" ~: TestList
-  ["renders the SVG drawing of a piece" ~:
-	 (render 0 0 b) ~?= 
-  		"<rect x=\"0\" y=\"0\" width=\"24\" height=\"24\" style=\"stroke-width:2;fill:#0000ff;stroke:#0000f0\"/>"
-  ,"renders pieces of different colors" ~:
-	 (render 0 0 r) ~?= 
-  		"<rect x=\"0\" y=\"0\" width=\"24\" height=\"24\" style=\"stroke-width:2;fill:#00ff00;stroke:#00f000\"/>"
+  ["renders the SVG drawing of a piece" ~: 
+   	(render 0 0 g) ~?= 
+  	 "<rect x=\"0\" y=\"0\" width=\"24\" height=\"24\" style=\"stroke-width:2;fill:#00ff00;stroke:#00f000\"/>"
+  ,"renders pieces of different colors" ~: TestList 
+  	[check $ (render 0 0 g) `includes` "#00ff00;stroke:#00f000" 
+  	,check $ (render 0 0 b) `includes` "#0000ff;stroke:#0000f0" 
+  	,check $ (render 0 0 r) `includes` "#ff0000;stroke:#f00000" 
+  	,check $ (render 0 0 y) `includes` "#ffff00;stroke:#f0f000"]
   ]
