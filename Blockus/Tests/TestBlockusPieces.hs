e12dfee32760ecb079d468c56227d0a0bb5b94dc
ToF- <cthibaut@octo.com>
2013-07-27 11:09:10 +0200

    refactoring

--- Blockus/Tests/TestBlockusPieces.hs
+++ Blockus/Tests/TestBlockusPieces.hs
@@ -5,63 +5,65 @@ import Blockus.Tests.TestUtils
 import Blockus.Shape
 import Blockus.BlockusPieces
 
-tests = TestList 
-  [check ((shapes!!0) !! 0 `equals` ["#"])
-  ,length (shapes!!0) ~?= 1
+tests = TestList
+  ["the small shape has only 1 position" ~: TestList  
+  	[check ((shapes!!0) !! 0 `equals` ["#"])
+    ,length (shapes!!0) ~?= 1]
+  ,"a bar shape has only 2 positions" ~: TestList  
+   [check ((shapes!!1) !! 0 `equals` ["##"])
+   ,check ((shapes!!1) !! 1 `equals` ["#",
+  									  "#"])
+   ,length (shapes!!1) ~?= 2
+   ,check ((shapes!!6) !! 0 `equals` ["####"])
+   ,check ((shapes!!6) !! 1 `equals` ["#",
+  									  "#",
+  									  "#",
+  									  "#"])
+   ,length (shapes!!6) ~?= 2]
+  
+  ,"a symmetric shape has only 4 positions" ~: TestList
+   [check ((shapes!!2) !! 0 `equals` ["##",
+    	                              " #"])
 
-  ,check ((shapes!!1) !! 0 `equals` ["##"])
+   ,check ((shapes!!2) !! 1 `equals` [" #",
+  	                                  "##"])
 
-  ,check ((shapes!!1) !! 1 `equals` ["#",
-  									 "#"])
-  ,length (shapes!!1) ~?= 2
+   ,check ((shapes!!2) !! 2 `equals` ["# ",
+  	                                  "##"])
 
-  ,check ((shapes!!2) !! 0 `equals` ["##",
-  	                                 " #"])
+   ,check ((shapes!!2) !! 3 `equals` ["##",
+   	                                  "# "])
+   ,length (shapes!!2) ~?= 4]
 
-  ,check ((shapes!!2) !! 1 `equals` [" #",
-  	                                 "##"])
+  ,"a non symmetric shape has 8 positions" ~: TestList
+  [check ((shapes!!7) !! 0 `equals` ["  #",
+  	                                  "###"])
 
-  ,check ((shapes!!2) !! 2 `equals` ["# ",
-  	                                 "##"])
+   ,check ((shapes!!7) !! 1 `equals` ["#",
+  									  "#",
+  	                                  "##"])
 
-  ,check ((shapes!!2) !! 3 `equals` ["##",
-  	                                 "# "])
-  ,length (shapes!!2) ~?= 4
+   ,check ((shapes!!7) !! 2 `equals` ["###",
+  	                                  "#  "])
 
-  ,check ((shapes!!3) !! 0 `equals` ["###"])
+   ,check ((shapes!!7) !! 3 `equals` ["##",
+  									  " #",
+  	                                  " #"])
 
-  ,check ((shapes!!3) !! 1 `equals` ["#",
-  	                                 "#",
-  	                                 "#"])
-  ,length (shapes!!3) ~?= 2
+   ,check ((shapes!!7) !! 4 `equals` ["#  ",
+  	                                  "###"])
 
-  ,check ((shapes!!7) !! 0 `equals` ["  #",
-  	                                 "###"])
+   ,check ((shapes!!7) !! 5 `equals` ["##",
+  									  "# ",
+  	                                  "# "])
 
-  ,check ((shapes!!7) !! 1 `equals` ["#",
-  									 "#",
-  	                                 "##"])
+   ,check ((shapes!!7) !! 6 `equals` ["###",
+  	                                  "  #"])
 
-  ,check ((shapes!!7) !! 2 `equals` ["###",
-  	                                 "#  "])
-
-  ,check ((shapes!!7) !! 3 `equals` ["##",
-  									 " #",
-  	                                 " #"])
-
-  ,check ((shapes!!7) !! 4 `equals` ["#  ",
-  	                                 "###"])
-
-  ,check ((shapes!!7) !! 5 `equals` ["##",
-  									 "# ",
-  	                                 "# "])
-
-  ,check ((shapes!!7) !! 6 `equals` ["###",
-  	                                 "  #"])
-
-  ,check ((shapes!!7) !! 7 `equals` [" #",
-  									 " #",
-  	                                 "##"])
+   ,check ((shapes!!7) !! 7 `equals` [" #",
+  									  " #",
+  	                                  "##"])
+   ,length (shapes!!7) ~?= 8]
 
   ]
 
