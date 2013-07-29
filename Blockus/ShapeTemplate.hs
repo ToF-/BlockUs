7016fdfe87a03dbfd4d08e440f2fad2f2f6938d0
ToF- <cthibaut@octo.com>
2013-07-30 01:56:40 +0200

    shape template with simplest shapes (no rotations/flip)

--- Blockus/ShapeTemplate.hs
+++ Blockus/ShapeTemplate.hs
@@ -5,4 +5,4 @@ import Blockus.Shape
 
 template :: [Shape] -> Shape
 template [] = sharp []
-template (s:ss) = s
+template (s:ss) = s `merge` (translate (0,area s + 1) $ template ss)
--- Blockus/Tests/TestShapeTemplate.hs
+++ Blockus/Tests/TestShapeTemplate.hs
@@ -8,10 +8,17 @@ import Blockus.ShapeTemplate
 
 tests = "A ShapeTemplate" ~: TestList
 	["collects at least 1 shape" ~: template [sharp ["#"]] ~?= sharp ["#"]
-	,"merge distinct shapes into one, with a line separation" ~:
+	,"merge 2 distinct shapes into one, with a line separation" ~:
 		template [sharp ["#"], sharp ["##","##"]] ~?= sharp ["# ",
 																												 "  ",
 																												 "##",
 																												 "##"]
+	,"merge N distinct shapes into one, with a line separation" ~:
+		template [sharp ["#"], sharp ["##","##"], sharp ["#"]] ~?= sharp ["# ",
+																												              "  ",
+																												              "##",
+																												              "##",
+																												              "  ",
+																												              "# "]
 	] 
 
