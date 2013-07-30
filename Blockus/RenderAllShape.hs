d188eff644a496799b73d46900c2aa7eb46d231c
ToF- <cthibaut@octo.com>
2013-07-30 22:43:49 +0200

    drawing template of all shapes -- clearer

--- Blockus/RenderAllShape.hs
+++ Blockus/RenderAllShape.hs
@@ -4,6 +4,9 @@ where
 import Blockus.SVGRenderer
 import Blockus.Piece
 import Data.ByteString.Char8 as BS
-import Blockus.BlockusPieces
+import Blockus.ShapeTemplate
 
-main = BS.putStrLn $ render $ Prelude.map (\((x,y),p) -> (x,y,p)) template
+main = BS.putStrLn $ renderSVG $ [(0,0,  Piece (template patterns) Blue)
+																 ,(50,0, Piece (template patterns) Red)
+																 ,(100,0, Piece (template patterns) Green)
+																 ,(150,0, Piece (template patterns) Yellow)]
--- Blockus/ShapeTemplate.hs
+++ Blockus/ShapeTemplate.hs
@@ -14,4 +14,70 @@ positions :: [Shape] -> Shape
 positions [] = empty
 positions (s:ss) = s `merge` (translate (area s + 1,0) $ positions ss) 
 
+patterns :: [Shape]
+patterns = [sharp ["#"]      -- 0
 
+            ,sharp ["##"]    -- 1
+
+            ,sharp ["##"     -- 2
+             			 ," #"]
+
+            ,sharp ["###"]   -- 3
+
+            ,sharp ["##"     -- 4
+             			 ,"##"]
+
+            ,sharp [" # "    -- 5
+             			 ,"###"]
+
+            ,sharp ["####"]  -- 6
+
+            ,sharp ["  #"    -- 7
+             			 ,"###"]
+
+            ,sharp [" ##"    -- 8
+             			 ,"## "]
+
+            ,sharp ["#   "   -- 9
+             			 ,"####"]
+
+            ,sharp [" # "    -- 10
+             			 ," # "
+             			 ,"###"]
+
+            ,sharp ["#  "    -- 11
+             			 ,"#  "
+             			 ,"###"]
+
+            ,sharp [" ###"   -- 12
+             			 ,"##  "]
+
+            ,sharp ["   #"   -- 13
+             			 ,"####"
+             			 ,"#   "]
+
+            ,sharp ["#####"] -- 14
+
+            ,sharp ["# "     -- 15
+             			 ,"##"
+             			 ,"##"]
+
+            ,sharp [" ##"    -- 16
+             			 ,"## "
+             			 ,"#  "]
+
+            ,sharp ["##"     -- 17
+             			 ,"# "
+             			 ,"##"]
+
+            ,sharp [" ##"    -- 18
+             			 ,"## "
+             			 ," # "]
+
+            ,sharp [" # "    -- 19
+             			 ,"###"
+             			 ," # "]
+
+            ,sharp [" #  "   -- 20 
+             			 ,"####"]
+            ]
